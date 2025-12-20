0d96f8ad6d426eafa90eb41617a08cd1854e0fb9
Abdullah Bajwa <abdullah.muhammad.bajwa@gmail.com>
2025-12-19 18:02:35 -0800

    Restore alpha.nvim dashboard
    Adds back the dragon ASCII art startup screen with quick action buttons.

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -1,26 +1,28 @@
 --[[
 ================================================================================
-  UI PLUGINS - STATUSLINE, ICONS, WHICH-KEY
+  UI PLUGINS - DASHBOARD, STATUSLINE, ICONS, WHICH-KEY
 ================================================================================
 
   These plugins improve Neovim's visual interface:
 
-  1. MINI.STATUSLINE
+  1. ALPHA-NVIM (Dashboard)
+     Shows a nice welcome screen when you open Neovim without a file.
+     Includes quick shortcuts to common actions.
+
+  2. MINI.STATUSLINE
      The bar at the bottom showing file info, mode, position, etc.
      Part of the mini.nvim collection.
 
-  2. MINI.AI & MINI.SURROUND
+  3. MINI.AI & MINI.SURROUND
      Enhanced text objects and surround operations.
      Part of the mini.nvim collection.
 
-  3. WHICH-KEY
+  4. WHICH-KEY
      Shows a popup with available keybindings when you start a key sequence.
      Press <leader> and wait to see what keys are available!
 
-  4. TODO-COMMENTS
+  5. TODO-COMMENTS
      Highlights special comments like TODO, FIXME, HACK, NOTE, etc.
-
-  NOTE: Dashboard is now provided by snacks.nvim (see plugins/snacks.lua)
 
 --]]
 
@@ -28,10 +30,83 @@
 -- Each item in the table is a separate plugin spec
 return {
   -- ==========================================================================
-  -- DASHBOARD
+  -- DASHBOARD (ALPHA-NVIM)
   -- ==========================================================================
-  -- Dashboard is now handled by snacks.nvim (see plugins/snacks.lua)
-  -- The dashboard shows when you open Neovim without arguments
+  -- Shows a pretty startup screen when you open Neovim without arguments.
+  -- Press the highlighted letter to activate each action.
+  {
+    'goolord/alpha-nvim',
+
+    -- This plugin needs icons
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+
+    config = function()
+      -- Load alpha and the dashboard theme
+      local alpha = require('alpha')
+      local dashboard = require('alpha.themes.dashboard')
+
+      -- HEADER: ASCII art shown at the top
+      -- Each string is one line of the art
+      -- Pro tip: Search "ASCII art generator" online to make your own!
+      dashboard.section.header.val = {
+        [[                             __                                ]],
+        [[                   _ ,___,-'",-=-.                             ]],
+        [[       __,-- _ _,-'_)_  (""`'-._\ `.                           ]],
+        [[    _,'  __ |,' ,-' __)  ,-     /. |                           ]],
+        [[  ,'_,--'   |     -'  _)/         `\                           ]],
+        [[,','      ,'       ,-'_,`           :                          ]],
+        [[,'     ,-'       ,(,-(              :                          ]],
+        [[     ,'       ,-' ,    _            ;                          ]],
+        [[    /        ,-._/`---'            /                           ]],
+        [[   /        (____)(----. )       ,'                            ]],
+        [[  /         (      `.__,     /\ /,                             ]],
+        [[ :           ;-.___         /__\/|                             ]],
+        [[ |         ,'      `--.      -,\ |                             ]],
+        [[ :        /            \    .__/                               ]],
+        [[  \      (__            \    |_                                ]],
+        [[   \       ,`-, *       /   _|,\                               ]],
+        [[    \    ,'   `-.     ,'_,-'    \                              ]],
+        [[   (_\,-'    ,'\")--,'-'       __\                             ]],
+        [[    \       /  // ,'|      ,--'  `-.                           ]],
+        [[     `-.    `-/ \'  |   _,'         `.                         ]],
+        [[        `-._ /      `--'/             \                        ]],
+        [[           ,'           |              \                       ]],
+        [[          /             |               \                      ]],
+        [[       ,-'              |               /                      ]],
+        [[      /                 |             -'                       ]],
+      }
+
+      -- BUTTONS: Quick actions on the dashboard
+      -- Format: dashboard.button(shortcut, icon + text, command)
+      dashboard.section.buttons.val = {
+        dashboard.button('f', '  Find file', ':FzfLua files<CR>'),
+        dashboard.button('e', '  New file', ':ene <BAR> startinsert<CR>'),
+        dashboard.button('r', '  Recent files', ':FzfLua oldfiles<CR>'),
+        dashboard.button('g', '  Find text', ':FzfLua live_grep<CR>'),
+        dashboard.button('c', '  Configuration', ':e $MYVIMRC<CR>'),
+        dashboard.button('l', '󰒲  Lazy', ':Lazy<CR>'),
+        dashboard.button('q', '  Quit', ':qa<CR>'),
+      }
+
+      -- FOOTER: Shows plugin load stats
+      -- This is a function because stats aren't available until lazy loads
+      dashboard.section.footer.val = function()
+        local stats = require('lazy').stats()
+        return '⚡ ' .. stats.loaded .. '/' .. stats.count .. ' plugins loaded'
+      end
+
+      -- COLORS: Set highlight groups for each section
+      dashboard.section.header.opts.hl = 'AlphaHeader'
+      dashboard.section.buttons.opts.hl = 'AlphaButtons'
+      dashboard.section.footer.opts.hl = 'AlphaFooter'
+
+      -- Activate alpha with our dashboard configuration
+      alpha.setup(dashboard.opts)
+
+      -- Disable code folding on the dashboard
+      vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+    end,
+  },
 
   -- ==========================================================================
   -- WHICH-KEY
